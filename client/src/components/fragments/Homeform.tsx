cafde04a3c7ee16c21d76f96797faadf265cb387
Zhibet <zhibet>
2025-02-27 00:49:40 -0500

    setup

--- client/src/components/fragments/Homeform.tsx
+++ client/src/components/fragments/Homeform.tsx
@@ -18,7 +18,7 @@ function Homeform(){
 
     return( 
         <form className="border-4 border-amber-800 w-full md:w-9/12 xl:w-5/12 mx-auto">
-            <h1>form goes here</h1>
+            <h1>form goes here, finish later</h1>
         </form>
     )
 }
